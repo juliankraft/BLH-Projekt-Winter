dd64a86988cfe677198a8b4d5ccb2ed2a8d8ec19
juliankraft <kraftjulian@gmail.com>
2023-11-13 01:39:09 +0100

    done for today

--- transform_bitelamina.R
+++ transform_bitelamina.R
@@ -1,23 +1,27 @@
 library(tidyverse)
-library(jsonlite)
-fromJSON("./data/raw/bitelamina/bitelamina.json") -> data_raw
+library(readxl)
 
-create_df <- function(str){
+import_sheet <- function(sheet) {
+colnames <- c("Loch", paste0("L", as.character(1:10)))
 
-    Wiederholung <- rep(str, 9) %>% as.factor()
+read_excel("./data/raw/bitelamina/Resultate_Koederstreifen.xlsx", sheet = sheet, skip = 4, col_names = colnames)%>% 
+    filter(Loch %in% as.character(1:16)) %>% 
+    select(! "L10") %>%
+    mutate(
+        Gruppe = paste0("G",sheet-1) %>% as.factor(),
+        Wiederholung = c(rep("W1", 16), rep("W2", 16), rep("W3", 16)) %>% as.factor(),
+        Tiefe = as.numeric(Loch) / 2
+        ) %>%
+    mutate_at(vars(L1:L9), as.numeric) %>%
+    select(Gruppe, Wiederholung, Tiefe, L1:L9) -> data
 
-    data_raw[[str]]  %>% as.tibble() %>% names() %>% as.factor() -> Nummer
-
-    seq(0.5,8,0.5) %>% as.character() -> new_col_names
-    data_raw[[str]]  %>% as.tibble() %>% t() %>% as.tibble(.name_repair = function(x) new_col_names) -> values
-
-    cbind(Wiederholung, Nummer, values) %>% as.tibble() -> df
-
-    return(df)
+    return(data)
 }
 
-data <- do.call(rbind, lapply(c("N", "M", "S"), create_df)) %>%
-    pivot_longer(-c(Wiederholung, Nummer), names_to = "Tiefe", values_to = "Wert") %>%
-    mutate(Tiefe = Tiefe %>% as.factor() %>% fct_rev())
-
-data %>% write_csv("./data/raw/bitelamina/bitelamina.csv")
+do.call(rbind, lapply(2:7, import_sheet)) %>%
+    pivot_longer(
+        -c(Gruppe, Wiederholung, Tiefe), 
+        names_to = "Streifen", 
+        values_to = "Wert"
+    ) %>% 
+    write_csv("./data/raw/bitelamina/bitelamina.csv")
